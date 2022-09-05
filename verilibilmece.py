f58abc72a4051f222eca5fc3d51e05ba5adefc9c
Can-Bey <cvtcn-ayglr4712@hotmail.com>
2022-09-06 02:59:16 +0300

    ikinci kayıt

--- verilibilmece.py
+++ verilibilmece.py
@@ -1,14 +1,26 @@
 import json
 from urllib.request import urlopen
 from random import randint
-bilmeceurl="https://raw.githubusercontent.com/canayglr/json/main/bilmeceler.json"
+bilmeceurl="https://raw.githubusercontent.com/canayglr/bulmaca/main/bilmeceler.json"
 deger=urlopen(bilmeceurl).read().decode("utf-8")
 jsonoku=json.loads(deger)
-sayi=randint(0,7)
 getdata=jsonoku["Bilmeceler"]
-print(getdata[sayi]["b"] + " Bilmecesinin Cevabı "+ getdata[sayi]["c"])
-cevap=input(getdata[sayi]["b"] + "\n")
-if cevap==getdata[sayi]["c"]:
-    print("Doğru Cevap!")
-else:
-    print("Cevap Yanlış!")
+hak=3
+input("Bilmece oyununa hoşgeldiniz, 3 yanlış hakkınız var devam etmek için herhangi bir tuşa basınız!")
+while True:
+    if hak==0:
+        print("Üzgünüm Dostum Elendiniz!")
+        break
+    else:
+        sayi=randint(0,7)
+        print(getdata[sayi]["b"] + " Bilmecesinin Cevabı "+ getdata[sayi]["c"])
+        cevap=input(getdata[sayi]["b"] + "\n")
+        if cevap==getdata[sayi]["c"]:
+            a=input("Soruyu doğru yanıtladınız çıkmak için [0] devam etmek için herhangi bir tuşa basınız.\n")
+            if a=="0":
+                print("Yarışma Sonlandırılmıştır!")
+                break
+                
+        else:
+            hak=hak-1
+            print(f"Soruya yanlış cevap verdiniz {hak} hakkınız kaldı!")
